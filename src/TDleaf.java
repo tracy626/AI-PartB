4195e47bb584b8be9bd7b146f1137f5bfad4a525
dodojk <tangzhe@TangMBP.local>
2017-05-10 21:58:07 +1000

    Start TDleaf

---
+++ src/TDleaf.java
@@ -0,0 +1,21 @@
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import aiproj.slider.Move.Direction;
+
+public class TDleaf {
+	public Integer[] w;
+	public ArrayList<Integer[]> features;
+	private double sumD = 0;
+	private static final double ALPHA = 0.1;
+	private int count = 0;
+	
+	public void detect_f(HashMap<Point, ArrayList<Direction>> curP, HashMap<Point, ArrayList<Direction>> opP, Direction dir) {
+		
+	}
+	
+	public void prove_w() {
+		
+	}
+}
